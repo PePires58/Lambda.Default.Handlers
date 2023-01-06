731439ed3ff43efce03866f4c60d5b5700db1073
Pedro Henrique Pires <pedrohenriquepires13@gmail.com>
2023-01-06 10:41:23 -0300

    fix: updating function to handle on success method when there are no notifications

--- src/Lambda.Default.Handler.EventBridge/DefaultLambdaHandlerEventBridge.cs
+++ src/Lambda.Default.Handler.EventBridge/DefaultLambdaHandlerEventBridge.cs
@@ -1,6 +1,5 @@
 ﻿using Amazon.Lambda.CloudWatchEvents;
 using Amazon.Lambda.Core;
-using Microsoft.Extensions.Logging;
 using Notification.Domain.Interfaces;
 
 namespace Lambda.Default.Handler.EventBridge
@@ -17,20 +16,13 @@ namespace Lambda.Default.Handler.EventBridge
         /// </summary>
         public abstract INotificatorService NotificatorService { get; set; }
 
-        /// <summary>
-        /// Logger
-        /// </summary>
-        ILogger<DefaultLambdaHandlerEventBridge<T>> Logger { get; set; }
-        
         #region Constructor
-        
+
         /// <summary>
         /// Default lambda handler for event bridge
         /// </summary>
-        /// <param name="logger">Logger object</param>
-        protected DefaultLambdaHandlerEventBridge(ILogger<DefaultLambdaHandlerEventBridge<T>> logger)
+        protected DefaultLambdaHandlerEventBridge()
         {
-            Logger = logger;
         }
         #endregion
 
@@ -43,42 +35,43 @@ namespace Lambda.Default.Handler.EventBridge
         /// <exception cref="ArgumentNullException">Throw argument null exception when the request or the detail is null</exception>
         public async Task DefaultLambdaHandlerForEventBridge(CloudWatchEvent<T> request, ILambdaContext context)
         {
-            Logger.LogInformation("Starting method");
+            context.Logger.LogInformation("Starting method");
 
             if (request == null || request.Detail == null)
             {
-                Logger.LogWarning("Request or detail is null");
+                context.Logger.LogWarning("Request or detail is null");
                 throw new ArgumentNullException(nameof(request));
             }
 
             try
             {
-                Logger.LogInformation("Starting service method");
+                context.Logger.LogInformation("Starting service method");
                 await CallServiceMethod(request.Detail);
-                Logger.LogInformation("Service method is done");
+                context.Logger.LogInformation("Service method is done");
 
                 if (NotificatorService.HasNotification)
                 {
-                    Logger.LogInformation("Starting OnSuccess method");
-                    await OnSuccess();
-                    Logger.LogInformation("On success completed");
+
+                    context.Logger.LogInformation("Starting on failure with notifications method");
+                    await OnFailureWithNotifications();
+                    context.Logger.LogInformation("On Failure with notifications completed");
                 }
                 else
                 {
-                    Logger.LogInformation("Starting on failure with notifications methods");
-                    await OnFailureWithNotifications(NotificatorService);
-                    Logger.LogInformation("Starting OnSuccess method");
+                    context.Logger.LogInformation("Starting OnSuccess method");
+                    await OnSuccess();
+                    context.Logger.LogInformation("On success completed");
                 }
             }
             catch (Exception ex)
             {
-                Logger.LogError(ex, "An error has occour");
+                context.Logger.LogError(ex.Message);
                 await OnError(ex);
                 throw;
             }
             finally
             {
-                Logger.LogInformation("End of the proccess");
+                context.Logger.LogInformation("End of the proccess");
                 NotificatorService.ClearNotifications();
             }
         }
@@ -88,21 +81,20 @@ namespace Lambda.Default.Handler.EventBridge
         /// </summary>
         /// <returns>Task</returns>
         public abstract Task OnSuccess();
-        
+
         /// <summary>
         /// On failure of your services, it if called if has some notifications
         /// </summary>
-        /// <param name="notificatorService">Notification service</param>
         /// <returns>Task</returns>
-        public abstract Task OnFailureWithNotifications(INotificatorService notificatorService);
-        
+        public abstract Task OnFailureWithNotifications();
+
         /// <summary>
         /// On error, in case of exception
         /// </summary>
         /// <param name="ex"></param>
         /// <returns></returns>
         public abstract Task OnError(Exception ex);
-        
+
         /// <summary>
         /// Call your services here
         /// </summary>
